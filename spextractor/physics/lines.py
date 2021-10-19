3eda3878f40ae4b5c5a15fe2aaf603b98b1dadd2
Anthony Burrow <anthony.r.burrow@gmail.com>
2021-10-19 08:37:35 -0500

    changed default lines

--- spextractor/physics/lines.py
+++ spextractor/physics/lines.py
@@ -1,41 +1,59 @@
 sn_lines = {
     'Ca II H&K': {'rest': 3945.12,
-                  'lo_range': (3450, 3800),
-                  'hi_range': (3800, 3950)},
-    'Si 4000A': {'rest': 4129.73,
-                 'lo_range': (3840, 3950),
-                 'hi_range': (4000, 4200)},
+                  'lo_range': (3500., 3750.),
+                  'hi_range': (3800., 4000.)},
+    'Si II 4000A': {'rest': 4129.73,
+                 'lo_range': (3800., 4000.),
+                 'hi_range': (4050., 4250.)},
     'Mg II 4300A': {'rest': 4481.2,
-                    'lo_range': (4000, 4250),
-                    'hi_range': (4300, 4700)},
+                    'lo_range': (4250., 4300.),
+                    'hi_range': (4300., 4420.)},
+    # 'Si III 4450A': {'rest': 4481.2,
+    #                 'lo_range': (4300., 4420.),
+    #                 'hi_range': (4500., 4600.)},
     'Fe II 4800A': {'rest': 5083.42,
-                    'lo_range': (4300, 4700),
-                    'hi_range': (4950, 5600)},
-    'S W': {'rest': 5536.24,
-            'lo_range': (5050, 5300),
-            'hi_range': (5500, 5750)},
+                    'lo_range': (4300., 4700.),
+                    'hi_range': (4950., 5600.)},
+    'S II 5500A': {'rest': 5536.24,
+            'lo_range': (5200., 5300.),
+            'hi_range': (5500., 5650.)},
     'Si II 5800A': {'rest': 6007.7,
-                    'lo_range': (5400, 5700),
-                    'hi_range': (5800, 6000)},
+                    'lo_range': (5600., 5750.),
+                    'hi_range': (5800., 6000.)},
     'Si II 6150A': {'rest': 6355.1,
-                    'lo_range': (5800, 6100),
-                    'hi_range': (6200, 6600)},
-    'Fe II': {'rest': 5169,
-              'lo_range': (4950, 5050),
-              'hi_range': (5150, 5250)},
-    'He I': {'rest': 5875,
-             'lo_range': (5350, 5450),
-             'hi_range': (5850, 6000)},
-    'O I': {'rest': 7773,
-            'lo_range': (7250, 7350),
-            'hi_range': (7750, 7950)}
+                    'lo_range': (5800., 6100.),
+                    'hi_range': (6250., 6500.)},
+    'O I 7500A': {'rest': 7773.,
+            'lo_range': (7250., 7350.),
+            'hi_range': (7500., 7750.)},
+    'Fe II': {'rest': 5169.,
+              'lo_range': (4950., 5050.),
+              'hi_range': (5150., 5250.)},
+    'He I': {'rest': 5875.,
+             'lo_range': (5350., 5450.),
+             'hi_range': (5850., 6000.)}
 }
 
 sn_types = {
-    'Ia': ('Ca II H&K', 'Si 4000A', 'Mg II 4300A', 'Fe II 4800A',
-           'S W', 'Si II 5800A', 'Si II 6150A'),
-    'Ib': ('Fe II', 'He I'),
-    'Ic': ('Fe II', 'O I')
+    'Ia' : (
+        'Ca II H&K',
+        'Si II 4000A',
+        'Mg II 4300A',
+        # 'Si III 4450A',
+        'Fe II 4800A',
+        'S II 5500A',
+        'Si II 5800A',
+        'Si II 6150A',
+        'O I 7500A'
+    ),
+    'Ib' : (
+        'Fe II',
+        'He I'
+    ),
+    'Ic' : (
+        'Fe II',
+        'O I 7500A'
+    )
 }
 
 
