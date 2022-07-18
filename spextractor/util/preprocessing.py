61c7f732fc9c7709b5df2c08d4dc916a310a3216
Anthony Burrow <anthony.r.burrow@gmail.com>
2022-07-18 14:08:40 -0500

    integrated ccm extinction correction

--- spextractor/util/preprocessing.py
+++ spextractor/util/preprocessing.py
@@ -1,13 +1,7 @@
 import numpy as np
 
 from ..physics.doppler import deredshift
-
-_can_deredden = True
-try:
-    from snpy.utils.deredden import unred
-except ModuleNotFoundError:
-    print('SNooPy not detected; unable to deredden spectra')
-    _can_deredden = False
+from ..physics.deredden import dered_ccm
 
 
 def preprocess(data, *args, **kwargs):
@@ -47,15 +41,12 @@ def prune(data, wave_range=None, *args, **kwargs):
 def deredden(data, host_EBV=None, host_RV=None, MW_EBV=None, MW_RV=3.1,
              *args, **kwargs):
     """Correct for extinction."""
-    if not _can_deredden:
-        return data
-
     # Milky Way extinction
     if MW_EBV is not None and MW_EBV != 0. and MW_RV is not None:
-        data[:, 1], _a, _b = unred(data[:, 0], data[:, 1], MW_EBV, R_V=MW_RV)
+        data[:, 1] = dered_ccm(data, E_BV=MW_EBV, R_V=MW_RV)
 
     # Host extinction
     if host_EBV is not None and host_EBV != 0. and host_RV is not None:
-        data[:, 1], _a, _b = unred(data[:, 0], data[:, 1], host_EBV, R_V=host_RV)
+        data[:, 1] = dered_ccm(data, E_BV=host_EBV, R_V=host_RV)
 
     return data
