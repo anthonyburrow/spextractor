d12ee3e386d54c5ed9a1913f3bf65296f40da564
Anthony Burrow <anthony.r.burrow@gmail.com>
2023-05-18 13:07:05 -0500

    fix boundary case

--- spextractor/util/preprocessing.py
+++ spextractor/util/preprocessing.py
@@ -42,9 +42,21 @@ def remove_telluric(data, remove_telluric=False, *args, **kwargs):
 
     for feature in telluric_to_remove:
         min_ind, max_ind = np.searchsorted(wave, feature)
-        all_inds = np.arange(min_ind, max_ind)
+        if min_ind == max_ind:
+            # Feature completely outside wavelengths, ignore it
+            continue
+        if min_ind == 0 or max_ind == len(wave):
+            # Spectrum begins/ends inside telluric, 
+            # remove instead since no line can be made
+            telluric_mask = (feature[0] <= wave) & (wave <= feature[1])
+            data = data[~telluric_mask]
+            continue
+
+        telluric_inds = np.arange(min_ind, max_ind)
+
+        min_ind -= 1
         slope = (flux[max_ind] - flux[min_ind]) / (wave[max_ind] - wave[min_ind])
-        flux[all_inds] = slope * (wave[all_inds] - wave[min_ind]) + flux[min_ind]
+        flux[telluric_inds] = slope * (wave[telluric_inds] - wave[min_ind]) + flux[min_ind]
 
     return data
 
